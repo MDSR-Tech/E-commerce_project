e3e06c483536c94764a00dfe8207273a404f3351
meron_daniel <dmeron29@gmail.com>
2025-11-22 06:38:30 -0500

    updated auth.py where email is collected aswell

--- backend/auth.py
+++ backend/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from backend.models import User
+from backend.models import Customer
 from backend.extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
@@ -9,17 +9,21 @@ auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
+    email = request.json.get('email')
     username = request.json.get('username')
-    password = request.json.get('password') # Extract 'password' from the request data and encode it
+    password = request.json.get('password') 
 
-    if not username or not password:
-        return jsonify({"error": "username and password required"}), 400
+    if not email or not username or not password:
+        return jsonify({"error": "email, username and password are required"}), 400
 
-    if User.query.filter_by(username=username).first():
+    if Customer.query.filter_by(username=username).first():
         return jsonify({'error' : 'username already exists'}), 409
+    
+    if Customer.query.filter_by(email=email).first():
+        return jsonify({'error' : 'email already exists'}), 409
 
     hashed_password = generate_password_hash(password)
-    new_user = User(username=username, password_hash=hashed_password)
+    new_user = Customer(email=email, username=username, password_hash=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
@@ -35,7 +39,7 @@ def login():
         return jsonify({'error': 'username and password required'}), 400
     
 
-    user = User.query.filter_by(username=username).first()
+    user = Customer.query.filter_by(username=username).first()
 
 
     if user or check_password_hash(user.password_hash, password):
